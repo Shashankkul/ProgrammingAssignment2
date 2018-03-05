64ce11a5d38f51b0c2875e8502ab34b03d0050da
Shashankkul <36036013+Shashankkul@users.noreply.github.com>
2018-03-05 21:52:52 +0530

    Create catcheSolve.R

---
+++ catcheSolve.R
@@ -0,0 +1,12 @@
+## Another function catchSolve
+cacheSolve <- function(x, ...) {
+  i <- x$getinverse()
+  if (!is.null(i)) {
+          message("getting cached data")
+          return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
+} ## End of the file
