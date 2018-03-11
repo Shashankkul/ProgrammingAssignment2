46debf3edf582d6379244e392c66aeec3c9aa69b
Shashankkul <36036013+Shashankkul@users.noreply.github.com>
2018-03-11 15:23:43 +0530

    Adding files to github
    To add 2 files to github

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,16 +1,13 @@
-## Write makecachematrix function
-## use <<- for assignment (note the difference between <- and <<-)
-makeCacheMatrix <- function(x = matrix()) {
-    i <- NULL ## Initialize to Null and set in next statement
-  set <- function(y) {
-          x <<- y
-          i <<- NULL
-  }
-  get <- function() x
-  setinverse <- function(inverse) i <<- inverse
-  getinverse <- function() i
-  list(set = set,
-       get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
-} ## End of the file
+makeCacheMatrix <- function(x = matrix()) { ## defining argument with default mode of matrix dtd 11 March 2018
+      inv <- NULL                             ## it is required to initialize inv as NULL
+    set <- function(y) {                    ## define the set function to assign new 
+          x <<- y       ## refer lapply lecture; value of matrix in environment refer to <- and <<-
+          inv <<- NULL                      
+      }
+      get <- function() x       
+     
+        setinverse <- function(inverse) inv <<- inverse  ## assigning value of inv
+        getinverse <- function() inv                     
+        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)  
+                                                                                      
+}
